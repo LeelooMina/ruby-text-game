d9874a38c2d63b3da2fa4f541ab9552971c50a22
Crystal Campbell <blankpage@gmail.com>
2023-01-10 17:47:42 -0600

    Chore: create item list & stats hash

---
+++ lib/items.rb
@@ -0,0 +1,11 @@
+item_stats = {
+    {sword: "Sword", atk: 3, desc: "A normal sword with a dirty blade. It's not as cool as Excalibur."},
+    {laptop: "Laptop", atk: 3, desc: "An old laptop. Can you even play Ruby Kingdom on this?"},
+    {wand: "Wand", atk: 3, desc: "A magic wand with beautiful carvings. No way you made this by yourself."},
+    {book: "Book", atk: 2, desc: "A mysterious book in an old language you can't understand. Wait, no, it's just in cursive."},
+    {candle: "Candle", atk: 1, desc: "A wax candle in a glass jar that says 'Live. Laugh, Love' Somehow it's already lit. "},
+    {paper: "Paper", atk: 1, desc: "A blank sheet of paper. Too bad pens don't exist in Ruby Kingdom."}
+}
+
+
+
