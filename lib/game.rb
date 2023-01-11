f81c2f7203069ee5c2d9d13bde3531a2d2457fd1
Crystal Campbell <blankpage@gmail.com>
2023-01-11 14:56:11 -0600

    Chore: refactor methods, add methods to set attr

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,29 @@
-require './lib/slow_text.rb'
-require './lib/character.rb'
+require "./lib/slow_text.rb"
+require "./lib/character.rb"
 
 class Game
+  def initialize
+    @current_character
+    @current_map
+    @combat_mode = false
+  end
 
-    def initialize
-            
-        @current_character
-        @current_map 
-        @combat_mode = false
-        
-    end
+def set_character(character)
+    @current_character = character
+end
 
-    def get_help 
-        puts "You really need help? Already?".red
-        puts <<-'HELP_TEXT'
+def set_map(map)
+     @current_map = map
+end
+
+def set_combat_mode(true_or_false)
+    @combat_mode = true_or_false
+end
+
+
+  def get_help
+    puts "You really need help? Already?".red
+    puts <<-'HELP_TEXT'
 
             Try typing:
             'Fight' to fight an enemy
@@ -23,58 +33,46 @@ class Game
 
 
         HELP_TEXT
+  end
 
+  def user_input(input)
+    case input
+    when input == "help"
+      get_help
+    when input == "go"
     end
+  end
 
-    def user_input(input)
-        case input
-        when input == "help"
-            get_help
-        when input ==
+  def start_combat(enemy)
+    @combat_mode = true
 
-        
-        end
+    while enemy.get_hp != 0
+      puts "Fight fight fight"
     end
+  end
 
-    def start_combat(enemy)
-        @combat_mode = true
-
-        while enemy.get_hp != 0
-            puts "Fight fight fight"
-        end
-
+  def start_game
+    puts "Type 'help' for... you'll never guess"
+    while @current_character.get_char_status == true
+      puts @current_map.get_location_desc
+      puts "What do you want to do next?"
+      input = gets.chomp
+      user_input(input.downcase)
     end
+  end
 
-    def start_game
-            puts "Type 'help' for... you'll never guess"
-        while @current_character.get_char_status == true
-
-            puts @current_map.get_location_desc
-            puts "What do you want to do next?"
-            input = gets.chomp
-            user_input(input.downcase)
-
-
-        end
-
+  def end_game
+    puts "Well, looks like you died. Good job!"
+    sleep 2
+    puts "Wanna try again?"
+    puts "You'll keep your level and items but I'm taking all #{$current_character.get_char_gold} of your gold."
+    puts "Y/N"
+    input = gets.chomp
+    if input.downcase == "y"
+      $current_character.revive_char
+      start_game
+    else
+      puts "Don't let the door hit you on the way out!"
     end
-    
-    def end_game
-        
-        puts "Well, looks like you died. Good job!"
-        sleep 2
-        puts "Wanna try again?"
-        puts "You'll keep your level and items but I'm taking all #{$current_character.get_char_gold} of your gold."
-        puts "Y/N"
-        input = gets.chomp
-        if input.downcase == "y"
-            $current_character.revive_char
-           start_game
-
-        else
-            puts "Don't let the door hit you on the way out!"
-        end
-    end
-
-
+  end
 end
