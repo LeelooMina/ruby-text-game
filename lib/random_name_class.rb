596808afe659231e8d7a867fade2eaf5f1b00f8e
Crystal Campbell <blankpage@gmail.com>
2023-01-11 14:53:40 -0600

    Chore: Add random name arrays & methods

---
+++ lib/random_name_class.rb
@@ -0,0 +1,178 @@
+## Generated with https://generatorfun.com/job-title-generator
+## From the site 1/11/2023 " Usage - You are free to use anything generated in your creative works. Because the generators use AI to create content it is possible it may create words or sentances that are owned by other parties. This is up to you to check. And as always, feel free to link back if you use our generators."
+
+$random_classes = [
+  "Lead Marketing Soothsayer",
+  "New Wave Brand Ninja",
+  "Genuine Research Architect",
+  "Zen Media Futurist",
+  "Outrageous Interaction Shaker",
+  "Lead Networking Troublemaker",
+  "Chief Productivity Mystic",
+  "Key Brand Planner",
+  "NextGen Avatar Maven",
+  "Emerging Buzz Instigator",
+  "Chief Dream Oracle",
+  "NextGen Futuring Maven",
+  "Chief Forward Zen Master",
+  "NextGen Media Virtualizer",
+  "NextGen Customer Zen Master",
+  "Forward Revelation VP",
+  "Serious User Strategizer",
+  "Global Scope Poobah",
+  "Senior Creativity Soothsayer",
+  "NextGen Dream Shaman",
+  "Genuine Thought Dude",
+  "Lead Brand Soothsayer",
+  "Serious Interaction Oracle",
+  "Emergent Integration Hero",
+  "New Wave Conversation Guru",
+  "Zen Integration Troublemaker",
+  "Lead Communications Oracle",
+  "Principal Alliance Navigator",
+  "Main Discovery Magician",
+  "Enterprise Discovery Maven",
+]
+
+$random_names = [
+  "Danna",
+  "Miracle",
+  "Thyme",
+  "Echo",
+  "Whisper",
+  "Sun",
+  "Belou",
+  "Thyme",
+  "Aden",
+  "Arwen",
+  "Ode",
+  "Echo",
+  "Laurel",
+  "Freedom",
+  "Truth",
+  "Nova",
+  "Grainne",
+  "Star",
+  "Belou",
+  "Grace",
+  "Leaf",
+  "Dancer",
+  "Silver",
+  "Fire",
+  "Remi",
+  "Aries",
+  "Angel",
+  "Genesis",
+  "Cypress",
+  "Mercury",
+]
+
+# random_monster_names = []
+
+random_monster_types = [
+  "Aerial Hobbit",
+  "Thunder Valkyrie",
+  "Boreal Satyr",
+  "Grave Fairy",
+  "Abyss Trogg",
+  "Phantom Harpy",
+  "Sand Dryads",
+  "Gilded Folk",
+  "Forest Fiend",
+  "Lost Dryad",
+  "Magma Trogg",
+  "Grizzly Imps",
+  "Hill Harpies",
+  "Grizzly Spriggan",
+  "Noble Trogg",
+  "Wretched Spriggan",
+  "Reserve Oceanids",
+  "Demi Hydra",
+  "Mimic Golem",
+  "Mountain Valkyrie",
+  "Blood Giant",
+  "Fire Folk",
+  "Pure Troll",
+  "Dark Elf",
+  "Supreme Gargoyle",
+  "Cursed Grendel",
+  "Swamp Dragonborn",
+  "Planar Spriggan",
+  "Serpent Faun",
+  "Spring Imp",
+  "Dark Beast",
+  "River Drak",
+  "Ethereal Kappa",
+  "Pinnacle Gargoyle",
+  "Dread Nymph",
+  "Boreal Dragonborn",
+  "Ancient Kobold",
+  "Bog Boggart",
+  "Demonic Gremlin",
+  "Occult Goliath",
+  "Ugly Abomination",
+  "Grim Vile",
+  "Barren Slime",
+  "Infamous Gall",
+  "Bold Dust",
+  "Creepy Horror",
+  "Giant Dread",
+  "Barren Herder",
+  "Agile Anomaly",
+  "Cold Deviation",
+  "Filthy Soul",
+  "Arid Deviation",
+  "Bruised Wailer",
+  "Disgusting Cavern Troll",
+  "Bewitched Gloom",
+  "Active Curse",
+  "Lanky Wailer",
+  "Lonely Flame",
+  "Twin Witch",
+  "Arid Umbra",
+  "Icy Mutt",
+  "Mad Terror",
+  "Horrible Soul",
+  "Anguished Cave Slime",
+  "Giant Terror",
+  "Jagged Mist",
+  "Broken Inferno",
+  "Empty Terror",
+  "White Ooze",
+  "Acidic Tainted Gloom",
+  "Hollow Razor",
+  "Corrupt Pest",
+  "Disfigured Dawn",
+  "Big Bad Charmer",
+  "Deadly Spirit",
+  "Faint Gut",
+  "Dangerous Fetid",
+  "Dismal Being",
+  "Meager Germ",
+  "Lonely Rust",
+
+]
+
+def random_name
+  end_range = $random_names.length - 1
+  ran_num = rand(0..end_range)
+  $random_names[ran_num]
+end
+
+def random_class
+  end_range = $random_classes.length - 1
+  ran_num = rand(0..end_range)
+  $random_classes[ran_num]
+end
+
+# def random_monster_name
+#   end_range = $random__monster_names.length - 1
+#   ran_num = rand(0..end_range)
+#   $random__monster_names[ran_num]
+# end
+
+def random_monster_type
+  end_range = $random_monster_types.length - 1
+  ran_num = rand(0..end_range)
+  $random_monster_types[ran_num]
+end
