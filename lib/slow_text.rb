50b5803be5f1cd3c9868ed597251e879ab13aadc
Crystal Campbell <blankpage@gmail.com>
2023-01-11 23:05:01 -0600

    Chore: code formatting

--- lib/slow_text.rb
+++ lib/slow_text.rb
@@ -1,4 +1,4 @@
 def slow_text(sleep_time = 0.05)
-    yield.each_char { |c| putc c; $stdout.flush; sleep sleep_time }
-    puts
-  end
+  yield.each_char { |c| putc c; $stdout.flush; sleep sleep_time }
+  puts
+end
