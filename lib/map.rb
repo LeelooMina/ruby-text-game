aa526fcb7d1c09e9c59222af741b8d6b23ed7ab7
Crystal Campbell <blankpage@gmail.com>
2023-01-17 10:25:12 -0600

    Chore: start building out mapping array

--- lib/map.rb
+++ lib/map.rb
@@ -1,3 +1,13 @@
+i = 0
+def evey_other
+    if rand(0..10).odd?
+        return "+"
+    end
+    
+    " "
+end
+
+empty_map_grid = Array.new(7) { Array.new(4, " +") }
 ## Gen map
 
 ## Items: House, tree, large rock, grass, one single castle, treasure chest
@@ -6,5 +16,17 @@
 
 ## Enter castle
 
+##
 
-## 
+empty_map_grid.each_with_index do |row, i|
+  row.each do |cell|
+    if i.even?
+    print " #{cell} "
+    elsif i.odd?
+        print " "
+    end
+   
+  end
+
+  puts " "
+end
