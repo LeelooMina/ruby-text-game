b47073d7391f5422bb4548f2060010bc24c9ccb4
Crystal Campbell <blankpage@gmail.com>
2023-01-17 16:45:43 -0600

    Test: issues with map spacing

--- lib/map.rb
+++ lib/map.rb
@@ -1,17 +1,26 @@
 class Map
   def initialize
-    @house = "H"
-    @tree = "T"
-    @large_rock = "R"
-    @ruby_castle = "C"
+    @house = "    H "
+    @tree = "   T "
+    @large_rock = "   R "
+    @ruby_castle = "   C "
+
+    @house_amt = 1
+    @castle_amt = 1
+    @tree_amt = 2
+    @rock_amt = 2
+
+    @empty_map_grid = Array.new(7) { Array.new(4, "")}
   end
 
-
-    ## Creates an arry to represent a 3x3 grid
+    ## generate_map
+    ## map is represented by an arry that represents a 3x3 grid
     ## map[rows][columns]
     ## Rows 0, 2, 4, 6 - Even numbers are the corners of the map's grid
     ## Rows 1, 3, 5 - Odd numbers have space for landmarks/ character placement
-    ## Columns 0, 1, 2 have space for landmarks/location
+    ## Row 7 is the bottom row of corners
+    ## Columns 0, 1, 3 have space for landmarks/location
+    ## Column 2 is left empty for spacing reasons
     ## Column 4 is the last column of map corners
     ##
     ## Example: Adding character location to center -  map[3][1]
@@ -23,36 +32,90 @@ class Map
    
     # +  +  +  + 
 
+# def generate_map
+
+#     @empty_map_grid[1][0] = @house
+#     @empty_map_grid[5][4] = @ruby_castle
+#     # @empty_map_grid.each_with_index do |row, i|
+#     #     row.each_with_index do |column, y|
+#     #       if i.odd? && i != 7
+#     #         @empty_map_grid[i][y] = landmark_counters
+#     #       end
+#     #     end
+#     # end
+#     # puts @empty_map_grid
+# end
     
+# def set_landmarks
+#     rand_num = rand(0..100)
 
-  empty_map_grid = Array.new(7) { Array.new(4, "") }
-  ## Gen map
+#     if rand_num.even? 
+#         return landmark_counters
+#     end  
 
-def get_landmark
+# end
 
-end
- 
+# def landmark_counters
+#     case
+#     when  @castle_amt != 0
+#         @castle_amt -= 1
+#         @castle
+#     when @house_amt != 0
+#         @house_amt -= 1
+#         @house
+#     when    @tree_amt != 0
+#         @tree_amt -= 1
+#         @tree
+#     when    @rock_amt != 0
+#         @rock_amt -= 1
+#         @rock
+#     end
+# end
 
-  ## Enter house
+# def get_landmark
 
-  ## Enter castle
+# end
 
-  ## Print Map
-
-  empty_map_grid.each_with_index do |row, i|
+def print_map
+     
+  @empty_map_grid.each_with_index do |row, i|
     row.each_with_index do |column, y|
       if i.even?
         print "  +"
-      elsif i == 3 && y == 1
-        space_counter = 0
-        while space_counter < y
-          print "   "
-          space_counter += 1
+      elsif column != "" && row != ""
+        if column != "" && row != ""
+            space_counter = 0
+            while space_counter < y
+              print "  "
+              space_counter += 1
+            end
+            print @empty_map_grid[i][y]
         end
-        print "   X "
+        # print "   X "
       end
     end
 
     puts " "
   end
 end
+
+## Spacing 
+
+def set_character_location(x, y)
+    @empty_map_grid[x][y] = "  X "
+end
+end
+  ## Enter house
+
+  ## Enter castle
+
+  ## Print Map
+
+  map = Map.new
+#   map.generate_map
+  map.set_character_location(5, 4)
+
+  map.print_map
+
+
+
