c056be40ab57730d495b33756368c1216c5f36ad
Crystal Campbell <blankpage@gmail.com>
2023-01-17 01:10:04 -0600

    Refactor: change redundant method & var names

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -6,54 +6,53 @@ require "./lib/pokemon_api.rb"
 class Enemy
   def initialize(current_character)
     @enemy_type
-    @enemy_name
-    @enemy_hp = 10
-    @enemy_loot = []
-    @enemy_gold = 1
-    @enemy_level = 1
-    @enemy_alive = true
-    @enemy_atk = 2
-    @enemy_worth_exp = 5
+    @name
+    @hp = 10
+    @loot = []
+    @gold = 1
+    @level = 1
+    @alive = true
+    @atk = 2
+    @worth_exp = 5
 
     decide_type
     set_enemy_level(current_character)
     set_init_hp
-    set_enemy_atk
-    set_init_hp
-    set_enemy_exp
+    set_atk
+    set_exp
     set_enemy_gold
   end
  
-  def get_enemy_name
-    @enemy_name
+  def get_name
+    @name
   end
 
-  def get_enemy_hp
-    @enemy_hp
+  def get_hp
+    @hp
   end
 
-  def get_enemy_loot
-    @enemy_loot
+  def get_loot
+    @loot
   end
 
-  def get_enemy_gold
-    @enemy_gold
+  def get_gold
+    @gold
   end
 
-  def get_enemy_level 
-    @enemy_level
+  def get_level 
+    @level
   end
 
-  def get_enemy_alive
-    @enemy_alive
+  def get_alive?
+    @alive
   end
 
   def get_enemy_atk
-    @enemy_atk
+    @atk
   end
 
-  def get_enemy_worth_exp
-    @enemy_worth_exp
+  def get_worth_exp
+    @worth_exp
   end
 
   ## Type
@@ -61,59 +60,59 @@ class Enemy
     rand_num = rand(0..20)
     if rand_num.odd?
       @enemy_type = "Fantasy"
-      set_enemy_name_fantasy
+      set_name_fantasy
     else
       @enemy_type = "Pokemon"
-      set_enemy_name_pokemon
+      set_name_pokemon
     end
   end
 
-  def set_enemy_name_fantasy
-    @enemy_name = random_monster_type
+  def set_name_fantasy
+    @name = random_monster_type
   end
 
-  def set_enemy_name_pokemon
-    @enemy_name = get_rand_pokemon_name
+  def set_name_pokemon
+    @name = get_rand_pokemon_name
   end
 
   ## Level
-  def set_enemy_level(character)
-    level_possible = character.get_chr_level + 1
+  def set_level(character)
+    level_possible = character.get_level + 1
     rand_num = rand(1..level_possible)
-    @enemy_level = rand_num
+    @level = rand_num
   end
 
-  def set_enemy_atk
-    @enemy_atk = 2 * @enemy_level
+  def set_atk
+    @atk = 2 * @level
   end
 
   ## Hit points
   def set_init_hp
-    @enemy_hp = @enemy_level * 10
+    @hp = @level * 10
   end
 
   def remove_hp(atk_amt)
-    @enemy_hp = @enemy_hp - atk_amt
-    check_enemy_status
+    @hp = @hp - atk_amt
+    check_status
   end
 
-  def check_enemy_status
-    if @enemy_hp <= 0
-      @enemy_alive = false
+  def check_status
+    if @hp <= 0
+      @alive = false
     end
   end
 
   ## Loot
-  def set_enemy_loot
+  def set_loot
 
   end
 
-  def set_enemy_gold
-    @enemy_gold = @enemy_level * 5
+  def set_gold
+    @gold = @level * 5
   end
 
   ## Exp
-  def set_enemy_exp
-    @enemy_worth_exp = @enemy_level * 5
+  def set_exp
+    @worth_exp = @level * 5
   end
 end
