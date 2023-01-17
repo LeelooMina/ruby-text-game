9cb24e2f1ad7e50d534ae08e63b10252c9ea4d8a
Crystal Campbell <blankpage@gmail.com>
2023-01-17 15:22:51 -0600

    Test: Print map rows w/ hardcode location

--- lib/map.rb
+++ lib/map.rb
@@ -7,7 +7,7 @@ def evey_other
     " "
 end
 
-empty_map_grid = Array.new(7) { Array.new(4, " +") }
+empty_map_grid = Array.new(7) { Array.new(4, "") }
 ## Gen map
 
 ## Items: House, tree, large rock, grass, one single castle, treasure chest
@@ -19,11 +19,16 @@ empty_map_grid = Array.new(7) { Array.new(4, " +") }
 ##
 
 empty_map_grid.each_with_index do |row, i|
-  row.each do |cell|
+  row.each_with_index do |cell, y|
     if i.even?
-    print " #{cell} "
-    elsif i.odd?
-        print " "
+    print "  +"
+    elsif y == 2 && i == 5
+        space_counter = 0
+        while space_counter < y 
+        print "   "
+        space_counter += 1
+        end
+        print "   X "
     end
    
   end
