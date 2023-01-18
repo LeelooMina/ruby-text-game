0866f2516beb6aa56ac1684b7bcf19d3e95d8e16
Crystal Campbell <blankpage@gmail.com>
2023-01-17 21:02:06 -0600

    Create menu.rb

---
+++ lib/menu.rb
@@ -0,0 +1,22 @@
+module Menu
+
+def self.run 
+    puts "Pick an option"
+    puts "1. Move"
+    puts "2. Fight"
+    puts "3. Check Inventory"
+    
+end
+
+def move 
+end
+
+def fight
+
+end
+
+def check_inventory
+
+end
+
+end
